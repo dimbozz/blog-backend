188c8eaee1bb94c91e41f90ac0c7c1bdbc55a596
Dmitriy <dmitriy.tsaberyabyy@kuper.ru>
2026-02-09 15:30:44 +0300

    Delete service/errors.go

--- service/errors.go
+++
@@ -1,9 +0,0 @@
-package service
-
-import "errors"
-
-var (
-	ErrUserNotFound = errors.New("user not found")
-	ErrPostNotFound = errors.New("post not found")
-	ErrInvalidInput = errors.New("invalid input data")
-)
